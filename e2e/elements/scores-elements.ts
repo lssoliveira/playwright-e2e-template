b8a01aebaac13af488b4c5f9aefc51faad4c91f7
lssoliveira <lucas.schmidtoliveira@hotmail.com>
2024-10-01 15:02:22 -0300

    change tab size

--- e2e/elements/scores-elements.ts
+++ e2e/elements/scores-elements.ts
@@ -1,25 +1,25 @@
 import { Locator, Page } from "@playwright/test";
 
 export class ScoreElements {
-    readonly page: Page;
+  readonly page: Page;
 
-    constructor(page: Page) {
-        this.page = page;
-    }
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-    getSearchButton(): Locator {
-        return this.page.locator('button[class*="site-header_search"]');
-    }
+  getSearchButton(): Locator {
+    return this.page.locator('button[class*="site-header_search"]');
+  }
 
-    getSearchField(): Locator {
-        return this.page.locator('input[class*="new-search-widget_input"]');
-    }
+  getSearchField(): Locator {
+    return this.page.locator('input[class*="new-search-widget_input"]');
+  }
 
-    getSearchResultByName(searchTerm: string): Locator {
-        return this.page.locator('div[class*="new-search-widget_entity_item_name"]', { hasText: searchTerm });
-    }
+  getSearchResultByName(searchTerm: string): Locator {
+    return this.page.locator('div[class*="new-search-widget_entity_item_name"]', { hasText: searchTerm });
+  }
 
-    getTitleText(): Locator {
-        return this.page.locator('h1[class="mega-header-module-entity-name"]');
-    }
+  getTitleText(): Locator {
+    return this.page.locator('h1[class="mega-header-module-entity-name"]');
+  }
 }
