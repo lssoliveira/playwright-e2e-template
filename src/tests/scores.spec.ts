b15b4381fc7b561dfedd2eaea04e3b4c712aea2a
lssoliveira <lucas.schmidtoliveira@hotmail.com>
2024-10-02 23:29:53 -0300

    add eslint

--- src/tests/scores.spec.ts
+++ src/tests/scores.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { ScoresPage } from '../pages/scores-page';
 import { ScoreData } from '../support/data/score-data';
 
